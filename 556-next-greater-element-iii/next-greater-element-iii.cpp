fea6e9439b5eb6613fbac16cc75fa908611aaf81
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2025-10-19 00:08:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (5.68%) - LeetSync

---
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void swap(char &a, char &b){
+        char temp = a;
+        a = b;
+        b = temp;
+    }
+    void find_ele(int i , string &s, int n){
+        int idx = i;
+        int temp = s[i+1] - 'a';
+        int src = s[i] - 'a';
+        for(int j = i+1 ; j < n ; j++){
+            if(src < (s[j] - 'a') && (s[j] - 'a') <= temp){
+                temp = s[j] - 'a';
+                idx = j;
+            }
+        }
+        cout<<"abc"<<endl;
+        swap(s[i], s[idx]);
+        sort(s.begin() + i + 1 , s.end());
+        return;
+    }
+
+    long nextGreaterElement(int n) {
+        stack<int> st;
+        string stt = to_string(n);
+        string temp = stt;
+        reverse(temp.begin(), temp.end());
+        int sz = stt.size();
+        bool check = false;
+        int i= sz-1;
+        while(i>=0){
+            if(!st.empty() && (stt[st.top()] - 'a') > (stt[i] - 'a')){
+                find_ele(i, stt, sz);
+                long ans = stol(stt);
+                cout<<ans<<endl;
+                return ans > INT_MAX ? -1 : ans;
+            }
+            if(st.empty() || (stt[st.top()] - 'a') <= (stt[i] - 'a')){
+                st.push(i);
+            }
+            i--;
+        }
+        return -1;
+    }
+};
