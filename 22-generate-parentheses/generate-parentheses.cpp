f0276070a05ae6a26be3d0b803296add7cb638b3
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2025-10-28 23:49:38 +0530

    Time: 95 ms (5.10%) | Memory: 104.2 MB (5.01%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
 
-    bool isValid(string s){
-        stack<char>  st;
-        // st.push(s[0]);
-        for(int i = 0 ; i < s.length(); i++){
-            if(s[i] == '('){
-                st.push(s[i]);
-            }
-            else if(!st.empty() && st.top() == '('){
-                st.pop();
-            }
-            else{
-                return false;
-            }
+    // bool isValid(string s){
+    //     stack<char>  st;
+    //     // st.push(s[0]);
+    //     for(int i = 0 ; i < s.length(); i++){
+    //         if(s[i] == '('){
+    //             st.push(s[i]);
+    //         }
+    //         else if(!st.empty() && st.top() == '('){
+    //             st.pop();
+    //         }
+    //         else{
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+
+    bool isValid(string s, stack<char> st, int i ){
+        if(i== s.size()){
+            return st.empty();
         }
-        return true;
-    }
+        if(s[i] == '('){
+            st.push('(');
+        }
+        else if(!st.empty() && st.top() == '('){
+            st.pop();
+        }
+        else{
+            return false;
+        }
+        return isValid(s, st, i+1);
+        // if(isValid(s, st, i+1)) return true;
+
+        // return true;
+    }   
 
     vector<string> generateParenthesis(int n) {
         string st;
@@ -28,18 +48,12 @@ public:
         st+=en;
         sort(st.begin(), st.end());
         vector<string> vec;
-        // vector<string> ans;
         do{
-            if(isValid(st))
+            stack<char> s;
+            if(isValid(st, s, 0))
                 vec.push_back(st);
         }
         while(next_permutation(st.begin(), st.end()));
-        // for(int i = 0 ; i < vec.size(); i++){
-        //     if(isValid(vec[i])){
-
-        //     }
-        //     cout<<vec[i]<<" ";
-        // }
         return vec;
     }
 };
