79aa3c7ea481e73913d527b54609064bd7e1a15a
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2025-10-29 00:03:35 +0530

    Time: 43 ms (5.10%) | Memory: 33 MB (5.25%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,59 +1,61 @@
 class Solution {
 public:
 
-    // bool isValid(string s){
-    //     stack<char>  st;
-    //     // st.push(s[0]);
-    //     for(int i = 0 ; i < s.length(); i++){
-    //         if(s[i] == '('){
-    //             st.push(s[i]);
-    //         }
-    //         else if(!st.empty() && st.top() == '('){
-    //             st.pop();
-    //         }
-    //         else{
-    //             return false;
-    //         }
+    bool isValid(string s){
+        stack<char>  st;
+        // st.push(s[0]);
+        for(int i = 0 ; i < s.length(); i++){
+            if(s[i] == '('){
+                st.push(s[i]);
+            }
+            else if(!st.empty() && st.top() == '('){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    // bool isValid(string s, stack<char> st, int i ){
+    //     if(i== s.size()){
+    //         return st.empty();
     //     }
-    //     return true;
-    // }
+    //     if(s[i] == '('){
+    //         st.push('(');
+    //     }
+    //     else if(!st.empty() && st.top() == '('){
+    //         st.pop();
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    //     return isValid(s, st, i+1);
+    //     // if(isValid(s, st, i+1)) return true;
 
-
-    bool isValid(string s, stack<char> st, int i ){
-        if(i== s.size()){
-            return st.empty();
+    //     // return true;
+    // }   
+    void solve(string s, vector<string> &ans, int idx){
+        if(idx == s.length()){
+            return ;
         }
-        if(s[i] == '('){
-            st.push('(');
+        // stack<char> st;
+        if(isValid(s)){
+        ans.push_back(s);}
+        if(next_permutation(s.begin(), s.end())){
+            solve(s, ans, idx);
         }
-        else if(!st.empty() && st.top() == '('){
-            st.pop();
-        }
-        else{
-            return false;
-        }
-        return isValid(s, st, i+1);
-        // if(isValid(s, st, i+1)) return true;
-
-        // return true;
-    }   
+    }
 
     vector<string> generateParenthesis(int n) {
-        string st;
-        string en;
-        for(int i = 0 ; i < n ; i++){
-            st+='(';
-            en+=')';
-        }
+        string st(n,'(');
+        string en(n,')');
         st+=en;
-        sort(st.begin(), st.end());
-        vector<string> vec;
-        do{
-            stack<char> s;
-            if(isValid(st, s, 0))
-                vec.push_back(st);
-        }
-        while(next_permutation(st.begin(), st.end()));
-        return vec;
+        vector<string> ans;
+
+        solve(st, ans, 0);
+        return ans;
     }
 };
