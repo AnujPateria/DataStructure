7015ca1ac375dc07ab45669e5b34327b1e7502df
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2025-11-01 00:07:52 +0530

    Time: 0 ms (100.00%) | Memory: 26.5 MB (15.56%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        unordered_map<int, int>mpp;
+        vector<int> ans;
+        for(int i = 0 ; i < nums.size(); i++){
+            mpp[nums[i]]++;
+            if(mpp[nums[i]] == 2){
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
