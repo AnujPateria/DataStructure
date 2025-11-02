00960793dc9219be68ceeb18bbb498f6866880b1
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2025-11-02 22:53:27 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (74.86%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        unordered_map<int, int>mpp;
-        vector<int> ans;
-        for(int i = 0 ; i < nums.size(); i++){
-            mpp[nums[i]]++;
-            if(mpp[nums[i]] == 2){
-                ans.push_back(nums[i]);
+        int XOR = 0;
+        for(int num : nums){
+            XOR^=num;
+        }
+        int n = nums.size() - 2;
+        for(int num = 0 ; num < n ; num++){
+            XOR^=num;
+        }
+        int trailing_zeroes = __builtin_ctz(XOR);
+        int mask = 1<<trailing_zeroes;
+
+        int g1 = 0;
+        int g2 = 0;
+        for(int num : nums){
+            if(num & mask){
+                g1^=num;
+            }
+            else{
+                g2^=num;
             }
         }
+        for(int num = 0 ; num< n ; num++){
+            if(num&mask){
+                g1^=num;
+            }
+            else{
+                g2^=num;
+            }
+        }
+        vector<int> ans;
+        ans.push_back(g1);
+        ans.push_back(g2);
         return ans;
     }
 };
