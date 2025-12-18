6fe9c46467b7bf6960bf1980e6f2fa37be1e8b07
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2025-12-19 00:13:18 +0530

    Time: 3 ms (28.30%) | Memory: 60.2 MB (48.22%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        vector<int> col;
-        int count = 0;
-        bool ch = true;
-        for(int i = 0 ; i < nums.size(); i++){
-            if(nums[i] == 0){
-                ch = false;
-                col.push_back(count);
-                col.push_back(0);
-                count = 0;
-            }
-            else{
-                count++;
-            }
-        }
-        if(count!=0){
-        col.push_back(count);}
-        if(ch){
-            return count-1;
-        }
+        //if all ones check seperately
+
         int ans = 0;
-        for(int i = 0 ; i < col.size(); i++){
-            if(col[i] == 0){
-                int temp = 0;
-                if(i-1 >=0 ){
-                    temp+=col[i-1];
-                }
-                if(i+1 < col.size()){
-                    temp+=col[i+1];
-                }
-                ans = max(ans , temp);
+        bool ch = true;
+        int prev = -1;
+
+        int left = 0;
+        int right = 0;
+
+        for(int i = 0 ; i < nums.size(); i++){
+
+            if(ch == false && nums[i] == 0){
+                left = right;
+                right = 0;
             }
+
+            else if(nums[i] != 0 && ch == true){
+                left++;
+            }
+            else if(nums[i] == 0){
+                ch = false;
+            }
+            else if(ch == false && nums[i]!=0){
+                right++;
+            }
+
+            ans = max(ans , left+right);
+        }
+        if(ch == true){
+            ans = left -1;
         }
         return ans;
 
+        
     }
 };
