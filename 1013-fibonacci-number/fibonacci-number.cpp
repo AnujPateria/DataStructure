b46d29a49c0f2feb0438aa00c51bcf291a6f7067
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2025-12-20 02:45:54 +0530

    Time: 14 ms (21.12%) | Memory: 7.9 MB (36.39%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        int x = 0 ;
-        int y = 1 ;
+        if(n==0 || n==1){return n;}
 
-        if(n==0 || n==1)
-            return n;
-        
-
-        for(int i = 2 ; i <= n; i++){
-            int ans  = x + y;
-            x = y;
-            y = ans;
-        }
-        return y;
+        return fib(n-1) + fib(n-2);
     }
 };
