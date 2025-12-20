7fe0ba0400bbd59be959b41ac705a64c197b7e0a
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2025-12-21 00:47:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.40%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
 
-    int find(int n , vector<int>&nums, vector<int> &dp){
-        if(n<0) return 0;
-        if(n==0){
-            return nums[n];
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        int first = nums[n] + find(n-2, nums, dp);
-        int second = find(n-1, nums ,dp);
-        return dp[n] = max(first, second);
-    }
+    // int find(int n , vector<int>&nums, vector<int> &dp){
+    //     if(n<0){
+    //         return 0;
+    //     }
+    //     if(n==0){
+    //         return nums[n];
+    //     }
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+    //     return dp[n] = max(nums[n] + find(n-2,nums, dp), find(n-1, nums, dp));
+    // }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, -1);
-        return find(n-1, nums, dp);
+        if(n==1){
+            return nums[0];
+        }
+        if(n==1){
+            return max(nums[0], nums[1]);
+        }
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        // if(n==1){
+        //     return dp[n];
+        // }
+
+        for(int i = 2 ; i < n ; i++){
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
+        }
+        return dp[n-1];
+
+
+        // return find(n-1, nums, dp);
     }
 };
