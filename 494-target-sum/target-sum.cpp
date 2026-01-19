ca237588e6f03f770ef08b18ad28656789b32317
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-01-19 14:24:30 +0530

    Time: 434 ms (22.13%) | Memory: 405.2 MB (5.00%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-
-    int dfs(int n, vector<int> &nums,int curr, int target){
+    vector<vector<int>>dp;
+    int sum = 0;
+    int dfs(int n, vector<int> &nums,int curr, int &target){
         if( n == 0 ){
             return curr == target;
         }
+        if(dp[curr + sum][n - 1] != -1){
+            return dp[curr + sum][n - 1];
+        }
         int take = dfs(n-1, nums, curr + nums[n-1], target);
         int notTake = dfs(n-1, nums, curr - nums[n-1], target);
-        return take + notTake;
+        return dp[curr + sum][n - 1] = take + notTake;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int curr = 0;
+        // int sum = 0;
+        for(int i = 0 ;i < nums.size(); i++){
+            sum+=(abs(nums[i]));
+        }
+        dp.assign(2* 1e3+1, vector<int> (2* 1e3+1, -1));
         return dfs(n, nums, curr, target);
     }
 };
