7513faa67f0de3b4688150c21d9763700c4bd993
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-01-29 08:24:37 +0530

    Time: 355 ms (5.05%) | Memory: 252.7 MB (5.28%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,49 +1,54 @@
 class Solution {
 public:
-    int n;
-    vector<vector<pair<int,int>>> dp;
-
-    // returns {maxLISLength, count}
-    pair<int,int> solve(int idx, int prev, vector<int>& nums) {
-        if (idx == n) {
-            return {0, 1};   // length 0, one valid way
+    vector<vector<pair<int, int>>> dp;
+    pair<int, int>  dfs(int idx, vector<int>&nums, int n, int prev, int &lcs, int count, int &ans){
+        if(n == idx){
+            return {0,1};
+            // if(lcs < count){
+            //     lcs = count;
+            //     ans = 1;
+            // }
+            // else if(lcs == count){
+            //     ans = ans+1;
+            // }
+            // return;
         }
-
-        if (dp[idx][prev + 1].first != -1) {
+        if(dp[idx][prev+1].first != -1){
             return dp[idx][prev + 1];
         }
-
-        // Option 1: NOT TAKE
-        auto notTake = solve(idx + 1, prev, nums);
-
-        // Option 2: TAKE (if valid)
-        pair<int,int> take = {0, 0};
-        if (prev == -1 || nums[prev] < nums[idx]) {
-            auto next = solve(idx + 1, idx, nums);
-            take.first = 1 + next.first;
-            take.second = next.second;
+        //NotTake;
+        auto notTake = dfs(idx+1, nums, n, prev, lcs, count, ans);
+        //Take
+        pair<int, int>  take = {0,0};
+        if(prev == -1 || nums[prev] < nums[idx]){
+            
+            auto next = dfs(idx+1, nums, n, idx, lcs, count+1, ans);
+            take.first = next.first + 1;
+            take.second = next.second;   
         }
-
-        // Choose best
-        pair<int,int> res;
-        if (take.first > notTake.first) {
+        pair<int, int> res;
+        if(take.first > notTake.first){
             res = take;
         }
-        else if (take.first < notTake.first) {
+        else if(take.first < notTake.first){
             res = notTake;
         }
-        else {
+        else{
             res.first = take.first;
-            res.second = take.second + notTake.second;
+            res.second = take.second+notTake.second;
         }
-
-        return dp[idx][prev + 1] = res;
+        return dp[idx][prev+1] = res;
+        // return;
     }
 
-    int findNumberOfLIS(vector<int>& nums) {
-        n = nums.size();
-        dp.assign(n, vector<pair<int,int>>(n + 1, {-1, -1}));
 
-        return solve(0, -1, nums).second;
+    int findNumberOfLIS(vector<int>& nums) {
+        int ans = 0;
+        int lcs = 0;
+        int n = nums.size();
+        int sum = (n*(n-1)) / 2;
+        dp.assign(n + 1, vector<pair<int, int>> (n+1, {-1, -1}));
+        return dfs(0, nums, nums.size(), -1, lcs, 0, ans).second;
+        // return ans;
     }
 };
