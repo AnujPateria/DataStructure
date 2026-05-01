20cd1d48485025a0fec351844a146c0457bb9458
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-05-01 15:17:02 +0530

    Time: 71 ms (91.65%) | Memory: 101.9 MB (79.24%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -1,54 +1,37 @@
 class Solution {
 public:
-    void merge(vector<int> &count, vector<pair<int, int> > &v, int left, int mid, int right) {
-        vector<pair<int, int> > tmp(right-left+1);
-        int i = left;
-        int j = mid+1;
-        int k = 0;
 
-        while (i <= mid && j <= right) {
-            // mind that we're sorting in descending order
-            if (v[i].first <= v[j].first) { 
-                tmp[k++] = v[j++];
-            }
-            else {
-                // only line responsible to update count, related to problem constraint, 
-                // remaining part is just regular mergeSort 
-                count[v[i].second] += right - j + 1;
-                tmp[k++] = v[i++];
-            }
-        }
-        while (i <= mid) {
-            tmp[k++] = v[i++];
-        }
-        while (j <= right) {
-            tmp[k++] = v[j++];
-        }
-        for (int i = left; i <= right; i++)
-            v[i] = tmp[i-left];
-    }        
+    vector<int> bit;
+    int n;
 
-    void mergeSort(vector<int> &count, vector<pair<int, int> > &v, int left, int right) {
-        if (left >= right) 
-            return;
-
-        int mid = left + (right-left)/2;
-        mergeSort(count, v, left, mid);
-        mergeSort(count, v, mid+1, right);
-        merge(count, v, left, mid, right);
+    void update(int add, int id){
+        while(id<=n){
+            bit[id] += add;
+            id += (id & (-id));
+        }
+    }
+    int query(int id){
+        int ans = 0;
+        while(id > 0){
+            ans += bit[id];
+            id -= (id & (-id));
+        }
+        return ans;
     }
 
     vector<int> countSmaller(vector<int>& nums) {
-        int N = nums.size();
-
-        vector<pair<int, int> > v(N);
-        for (int i = 0; i < N; i++)   
-            v[i] = make_pair(nums[i], i);
-
-        vector<int> count(N, 0);
-        // sorting in descending order
-        mergeSort(count, v, 0, N-1);
-
-        return count;
+        n = nums.size();
+        bit = vector<int> (n+1, 0);
+        vector<pair<int, int>> v;
+        for(int i = 0 ; i < n ; i++){
+            v.push_back({nums[i], i});
+        }
+        sort(v.begin(), v.end());
+        vector<int> ans(n, 0);
+        for(auto &[el,id] : v){
+            update(1, id + 1);
+            ans[id] = query(n) - query(id + 1);
+        }
+        return ans;
     }
 };
