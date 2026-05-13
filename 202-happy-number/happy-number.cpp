77dd1b2aa2b98ee996b23067834bd524e370ebef
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-05-13 11:31:33 +0530

    Time: 11 ms (2.53%) | Memory: 8.4 MB (37.49%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     bool isHappy(int n) {
         unordered_map<int, int> mp;
-        // long long sum = 0;÷
-        // long long nn = n;
         while(true){
-            long long sum = 0;
+            int sum = 0;
             while(n>=1){
                 int temp = n % 10;
                 n = n/10;
-                sum = 1LL*sum + 1LL*pow(temp,2);
+                sum = sum + pow(temp,2);
             }
 
             
