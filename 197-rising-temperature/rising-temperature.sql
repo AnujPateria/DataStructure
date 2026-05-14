a211aa525ab59e9af9311e4f5f523cfcca06cef5
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-05-14 13:32:21 +0530

    Time: 643 ms (8.99%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,2 +1,9 @@
 # Write your MySQL query statement below
-select w1.id from Weather as w1 where w1.temperature > (select w2.temperature from Weather as w2 where w2.recordDate = DATE_SUB(w1.recordDate, interval 1 day ))
+-- select w1.id from Weather as w1 where w1.temperature > (select w2.temperature from Weather as w2 where w2.recordDate = DATE_SUB(w1.recordDate, interval 1 day ))
+
+select w1.id from Weather as w1 inner join Weather as w2 on  DATEDIFF(w1.recordDate,  w2.recordDate) = 1
+where w1.temperature > w2.temperature
+
+
+-- select w1.id from Weather as w1 INNER JOIN Weather as w2 on DATEDIFF(w1.recordDate , w2.recordDate) = 1 
+-- where w1.temperature > w2. temperature
