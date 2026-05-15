2eac945831580471264c31fac50fdba7059680c1
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-05-15 11:07:00 +0530

    Time: 635 ms (21.14%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,2 +1,15 @@
 # Write your MySQL query statement below
-select customer_id from Customer as c group by customer_id having count(distinct product_key) = (select count(*) from Product);  
+-- select customer_id from Customer as c group by customer_id join Product 
+-- having count(distinct c.product_id)  = c
+
+select c1.customer_id from Customer c1  
+cross join Product p 
+left join Customer c2
+on c1.customer_id = c2.customer_id
+and 
+c2.product_key = p.product_key
+group by c1.customer_id
+having count(c2.product_key) = count(p.product_key);
+
+
+
