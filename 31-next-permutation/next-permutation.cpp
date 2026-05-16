f2c07ba88e1cfbd1ca9567c51194d649e357c739
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-05-17 01:59:45 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (31.93%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
-
-    void sortArray(vector<int> &nums, int st, int en){
-        for(int i = st; i < en; i++){
-            for(int j = i + 1; j<=en; j++){
-                if(nums[i] > nums[j]){
-                    cout<<nums[i]<<" "<<nums[j]<<endl;
-                    swap(nums[i], nums[j]);
-                }
-            }
-        }
-    }
-
-    void checkIndex(vector<int> &nums, int st, int en){
-        // bool check = false;
-        for(int i = en; i>=1; i--){
-            if(nums[i] > nums[i-1]){
-                int max_ele = i;
-                
-                for(int j = en; j>=i ; j--){
-                    if(nums[i-1] < nums[j] && nums[max_ele] > nums[j]){
-                        max_ele = j;
-                    }
-                }
-                cout<<nums[i-1]<<endl<<nums[max_ele]<<endl;
-                cout<<nums[i]<<endl;
-                swap(nums[i-1], nums[max_ele]);  // just greater than i- 1 then swap with that element;
-                cout<<nums[i-1]<<endl<<nums[max_ele];
-                sortArray(nums, i, en);
-                return;
-            }
-        }
-        sort(nums.begin(), nums.end());
-
-    }
-
     void nextPermutation(vector<int>& nums) {
-        checkIndex(nums, 0, nums.size() - 1);
-
+        int n = nums.size();
+        
+        // 1. Find first decreasing element (pivot)
+        int i = n - 2;
+        while(i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        
+        // 2. If found, find just greater element and swap
+        if(i >= 0) {
+            int j = n - 1;
+            while(nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+        
+        // 3. Reverse the suffix
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
