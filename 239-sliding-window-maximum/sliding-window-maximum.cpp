0a6fe38af5ffe03141c9bb79e004e7f92404b765
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-05-26 10:02:52 +0530

    Time: 18 ms (77.51%) | Memory: 139.1 MB (66.68%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
-
-    vector<int> seg;
-    vector<int> arr;
-
-    void build(int idx, int low, int high){
-        if(low == high){
-            seg[idx] = arr[low];
-            return ;
-        }
-        int mid = (low+high)/2;
-        build(2*idx + 1, low, mid);
-        build(2*idx + 2, mid + 1, high);
-        seg[idx] = max(seg[2*idx + 1], seg[2*idx + 2]);
-    }
-
-    int query(int idx, int low, int high, int i, int j){
-        if( i > high || j < low){
-            return INT_MIN;
-        }
-        if(j >= high && i <= low){
-            return seg[idx];
-        }
-        int mid = (low + high)/2;
-        int left = query(2*idx + 1, low, mid, i, j);
-        int right = query(2*idx + 2, mid + 1, high, i, j);
-        return max(left, right);
-    }
-
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        seg.assign(4*n, INT_MIN);
+    vector<int> maxSlidingWindow(vector<int>& tt, int k) {
+        int n = tt.size();
+        if (n == 0) return {};
+        
         vector<int> ans;
-        arr = nums;
-        build(0, 0, n-1);
-        for(int i = 0; i <= n - k ; i++){
-            int res = query(0, 0, n-1, i, k + i - 1);
-            ans.push_back(res);
+        deque<int> d; // This will store INDICES
+        //front has big end has small
+        for(int i = 0; i < n; i ++){
+            while (!d.empty() && tt[i] > tt[d.back()]){
+                d.pop_back();
+            }
+            d.push_back(i);
+            if (i >= k-1){
+                //the window has k elements now - have to remove i - k element
+                if (d.front() == i - k){
+                    d.pop_front();
+                }
+            ans.push_back(tt[d.front()]);
+            }
         }
         return ans;
+
     }
 };
