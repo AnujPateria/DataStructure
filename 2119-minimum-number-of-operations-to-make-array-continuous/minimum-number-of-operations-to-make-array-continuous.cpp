a00f853b26da3ec1aaeb6c54aa8efe351fc30137
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-06-02 10:01:21 +0530

    Time: 31 ms (98.96%) | Memory: 68 MB (89.64%) - LeetSync

--- 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int n=nums.size();
-        auto last=unique(nums.begin(), nums.end());
+        int n = nums.size();
+        auto last = unique(nums.begin(), nums.end());
         nums.erase(last, nums.end());
-
-        int left=0, maxcnt=0;
-        for(int right=0; right<nums.size(); right++){
-            while(nums[right]-nums[left]>=n){
+        int left = 0, maxcnt = 0;
+        for(int right = 0 ; right < nums.size(); right++){
+            while(nums[right] - nums[left] >= n){
                 left++;
             }
-            maxcnt=max(maxcnt, right-left+1);
+            maxcnt = max(maxcnt , right - left + 1);
         }
-        return n-maxcnt;
-        
+        return n - maxcnt;
+
     }
 };
