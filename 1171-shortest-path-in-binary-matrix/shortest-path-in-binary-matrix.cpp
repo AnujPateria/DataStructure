4e4cf7d06924df9e20cadd8d4beda5a8a2d7049d
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-06-15 15:56:11 +0530

    Time: 37 ms (21.62%) | Memory: 24.7 MB (63.13%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> dis(n, vector<int>(n, INT_MAX));
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
+            return -1;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
+                       greater<tuple<int, int, int>>>
+            pq;
+        pq.push({1, 0, 0});
+        int dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+        int dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+        while (pq.size()) {
+            auto [d, x, y] = pq.top();
+            pq.pop();
+            if (d > dis[x][y])
+                continue;
+            for (int i = 0; i < 8; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n &&
+                    grid[nx][ny] == 0 && d + 1 < dis[nx][ny]) {
+                    dis[nx][ny] = 1 + d;
+                    if (nx == n - 1 && ny == n - 1)
+                        return dis[nx][ny];
+                    pq.push({dis[nx][ny], nx, ny});
+                }
+            }
+        }
+        return -1;
+    }
+};
