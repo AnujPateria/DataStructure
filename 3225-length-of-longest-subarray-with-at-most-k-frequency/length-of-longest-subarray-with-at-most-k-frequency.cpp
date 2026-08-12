609d9f370fca3c4def5253deaa1044d3526389b5
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-08-12 13:57:17 +0530

    Time: 55 ms (90.73%) | Memory: 149.5 MB (27.87%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -7,7 +7,7 @@ public:
         for(int i = 0 ; i < nums.size(); i++){
             mp[nums[i]]++;
             if(mp[nums[i]] == k + 1){
-                while(st < i && mp[nums[i]] == k + 1){
+                while(mp[nums[i]] == k + 1){
                     mp[nums[st++]]--;
                 }
             }
