5ac7c508cdce569beaae7007177125814ad4d456
Anuj Pateria <152527035+AnujPateria@users.noreply.github.com>
2026-09-14 00:22:52 +0530

    Time: 5 ms (55.58%) | Memory: 19.2 MB (82.18%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    bool canFinish(int nc, vector<vector<int>>& pre) {
-        vector<vector<int>>adj(nc);
+    bool canFinish(int n, vector<vector<int>>& pr) {
+        vector<int> indeg(n, 0);
+        vector<vector<int>> adj(n);
 
-        vector<int> indeg(nc, 0);
-
-        for(auto ele : pre){
-            adj[ele[1]].push_back(ele[0]);
-            indeg[ele[0]]++;
+        queue<int> pq;
+        for(int i = 0 ; i< pr.size(); i++){
+            int x = pr[i][0]; int y = pr[i][1];
+            adj[y].push_back(x);
+            indeg[x]++;
         }
-
-        queue<int> qu;
-        for(int i = 0 ; i < nc ; i++){
+        for(int i = 0 ; i< n; i++){
             if(indeg[i] == 0){
-                qu.push(i);
+                pq.push(i);
             }
         }
         int count = 0;
-        while(!qu.empty()){
-            int node = qu.front();
-            qu.pop();
+        while(!pq.empty()){
+            int top = pq.front(); pq.pop();
             count++;
-            for(auto it: adj[node]){
-                indeg[it]--;
-                if(indeg[it] == 0){
-                    qu.push(it);
+            for(auto node : adj[top]){
+                indeg[node]--;
+                if(indeg[node] == 0){
+                    pq.push(node);
                 }
             }
+
         }
-        return count == nc;
+        return count == n;
     }
 };
